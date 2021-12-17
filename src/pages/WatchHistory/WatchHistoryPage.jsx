2eae1cc84bf4c0f24968860d3322741439957734
Mykhailo Beznos <mykhailo.beznos@ukad-group.com>
2021-12-17 14:55:27 +0200

    first commit

--- src/pages/WatchHistory/WatchHistoryPage.jsx
+++ src/pages/WatchHistory/WatchHistoryPage.jsx
@@ -17,28 +17,10 @@ const CreateProductPage = () => {
                     </thead>
                     <tbody>
                         <tr>
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>
-                        <tr>
-                            <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                        </tr>
-                        <tr>
-                            <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                        </tr>
-                        <tr>
-                            <th>4</th>
-                            <td>Marjy Ferencz</td>
-                            <td>Office Assistant I</td>
-                            <td>Crimson</td>
+                            <th></th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
                         </tr>
                     </tbody>
                 </table>
